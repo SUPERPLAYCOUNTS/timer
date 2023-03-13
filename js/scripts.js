70017a31c722b57dd22ee757f8558e418e895ac6
SUPERPLAYCOUNTS <85057670+SUPERPLAYCOUNTS@users.noreply.github.com>
2023-03-13 18:55:37 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,12 @@ const themesDropdown = document.querySelector('.themes');
 const savedTheme = localStorage.getItem('theme') || 'light'; // use the saved theme or default to light
 document.documentElement.classList.add(savedTheme);
 
+// find the radio button that matches the saved theme
+const selectedRadio = themesDropdown.querySelector(`input[value="${savedTheme}"]`);
+if (selectedRadio) {
+  selectedRadio.checked = true;
+}
+
 themesDropdown.addEventListener('change', (event) => {
   if (event.target.nodeName === 'INPUT') {
     document.documentElement.classList.remove('dark', 'light');
