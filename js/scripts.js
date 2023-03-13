1d00e1af3657b9384e34050b4ed9b387d18282a2
SUPERPLAYCOUNTS <85057670+SUPERPLAYCOUNTS@users.noreply.github.com>
2023-03-13 18:50:45 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,9 @@
 const themesDropdown = document.querySelector('.themes');
-const selectedTheme = localStorage.getItem('theme') || 'light'; // use the saved theme or default to light
-
-// set the initial theme
-document.documentElement.classList.add(selectedTheme);
+const savedTheme = localStorage.getItem('theme') || 'light'; // use the saved theme or default to light
+document.documentElement.classList.add(savedTheme);
 
 themesDropdown.addEventListener('change', (event) => {
   if (event.target.nodeName === 'INPUT') {
-    console.log('asdas');
     document.documentElement.classList.remove('dark', 'light');
     document.documentElement.classList.add(event.target.value);
     localStorage.setItem('theme', event.target.value); // save the selected theme to localStorage
