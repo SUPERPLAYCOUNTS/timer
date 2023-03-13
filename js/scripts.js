a4c175f16eb09f5aba2fd9be03f9ca8b3cd4a031
SUPERPLAYCOUNTS <85057670+SUPERPLAYCOUNTS@users.noreply.github.com>
2023-03-13 18:46:49 +0200

    Saving for changing the website's theme

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
-document.querySelector('.themes').addEventListener('change', (event) => {
-   if (event.target.nodeName === 'INPUT') {
-   	console.log('asdas');
-   	document.documentElement.classList.remove('dark', 'light');
-   	document.documentElement.classList.add(event.target.value);
-   }
+const themesDropdown = document.querySelector('.themes');
+const selectedTheme = localStorage.getItem('theme') || 'light'; // use the saved theme or default to light
+
+// set the initial theme
+document.documentElement.classList.add(selectedTheme);
+
+themesDropdown.addEventListener('change', (event) => {
+  if (event.target.nodeName === 'INPUT') {
+    console.log('asdas');
+    document.documentElement.classList.remove('dark', 'light');
+    document.documentElement.classList.add(event.target.value);
+    localStorage.setItem('theme', event.target.value); // save the selected theme to localStorage
+  }
 });
